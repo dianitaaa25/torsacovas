96b5ada74850ae5be36075de88b6beb3d67c455b
Samil Acosta <159671384+dianitaaa25@users.noreply.github.com>
2026-01-15 17:49:21 -0400

    Delete js directory

--- js/loadPartials.js
+++
@@ -1,9 +0,0 @@
-async function loadPartial(id, file) {
-  const response = await fetch(file);
-  const data = await response.text();
-  document.getElementById(id).innerHTML = data;
-}
-
-loadPartial("header", "partials/header.html");
-loadPartial("menu", "partials/menu.html");
-loadPartial("footer", "partials/footer.html");
