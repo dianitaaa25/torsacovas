1f9916223e40b561345c89a289e36a0c8c9827e5
Samil Acosta <159671384+dianitaaa25@users.noreply.github.com>
2026-01-15 17:44:59 -0400

    Create load-partials.js

---
+++ assets/js/load-partials.js
@@ -0,0 +1,12 @@
+function loadPartial(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    })
+    .catch(err => console.error(`Error cargando ${file}`, err));
+}
+
+loadPartial("header", "partials/header.html");
+loadPartial("menu", "partials/menu.html");
+loadPartial("footer", "partials/footer.html");
