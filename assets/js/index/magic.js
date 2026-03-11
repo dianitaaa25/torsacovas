024cfcc3276c307d5c91ef4df1b6360dcd807a34
Héctor Samil Acosta <hectorsamilacostarivas@gmail.com>
2026-03-10 22:22:05 -0400

    404 page updated

--- assets/js/index/magic.js
+++ assets/js/index/magic.js
@@ -2,13 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const enterBtn = document.getElementById("enterBtn");
 
   if (enterBtn) {
-    enterBtn.addEventListener("click", () => {
+    enterBtn.addEventListener("click", (e) => {
+      e.preventDefault(); 
       const siteWrapper = document.querySelector(".site-wrapper");
 
       if (siteWrapper) siteWrapper.classList.add("magic-opening");
 
+      const destination = enterBtn.getAttribute("href");
+
       setTimeout(() => {
-        window.location.href = "/biografia";
+        window.location.href = destination;
       }, 900);
     });
   }
